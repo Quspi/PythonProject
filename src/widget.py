de3731aba04ebc2b06cb5ddc23e841e0fca6728c
Oleg Tamanov <olegtamanov@gmail.com>
2025-11-15 17:23:30 +0300

    improve validation for get_date func

--- src/widget.py
+++ src/widget.py
@@ -23,4 +23,11 @@ def mask_account_card(account_card: str) -> str:
 
 def get_date(data: str) -> str:
     """Функция, которая преобразует дату из ISO формата в формат ДД.ММ.ГГГГ."""
+    data = data.strip()
+
+    if len(data) < 10:
+        raise ValueError("Строка слишком короткая для даты в формате ГГГГ-ММ-ДД")
+    if data[4] != "-" or data[7] != "-":
+        raise ValueError("Неверный формат даты. Ожидается: ГГГГ-ММ-ДД")
+
     return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
