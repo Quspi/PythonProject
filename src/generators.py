3b278a1e6862e219982d5fe9838e302c2313419b
Oleg Tamanov <olegtamanov@gmail.com>
2025-11-23 21:25:15 +0300

    add transaction_descriptions generator

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,13 @@
-from typing import Iterator
+from typing import Iterator, Optional
 
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
     """Функция, которая принимает на вход данные о транзакциях и возвращает итератор транзакций с указанной валютой"""
     return (item for item in transactions if item["operationAmount"]["currency"]["code"] == currency)
+
+
+def transaction_descriptions(transactions: list[dict]) -> Iterator[Optional[str]]:
+    """Функция-генератор, которая принимает на вход данные о транзакциях
+    и возвращает описание транзакций по очереди."""
+    for item in transactions:
+        yield item.get("description")
