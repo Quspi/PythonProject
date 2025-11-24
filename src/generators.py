dcfbd81d6a83a347f86098ad2e0736348f2dfba1
Oleg Tamanov <olegtamanov@gmail.com>
2025-11-24 18:41:32 +0300

    add card_number_generator function

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Optional
+from typing import Generator, Iterator, Optional
 
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
@@ -11,3 +11,11 @@ def transaction_descriptions(transactions: list[dict]) -> Iterator[Optional[str]
     и возвращает описание транзакций по очереди."""
     for item in transactions:
         yield item.get("description")
+
+
+def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
+    """Генерирует номера карт в формате "XXXX XXXX XXXX XXXX" в диапазоне от start до stop."""
+    for number in range(start, stop + 1):
+        card_number_str = str(number).zfill(16)
+        formated_card_number = " ".join([card_number_str[i : i + 4] for i in range(0, 16, 4)])
+        yield formated_card_number
