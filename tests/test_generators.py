ef2aea4f8e815636ac4bc3232db6be827ca060d1
Oleg Tamanov <olegtamanov@gmail.com>
2025-11-24 19:25:49 +0300

    add tests for card_number_generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,7 +2,7 @@ from typing import Iterator
 
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 # Тестирование функции filter_by_currency
@@ -92,3 +92,69 @@ def test_empty_list(empty_transactions):
     gen = transaction_descriptions(empty_transactions)
     assert isinstance(gen, Iterator)
     assert list(gen) == []
+
+
+# Тестирование функции-генератора card_number_generator
+
+
+def test_card_number_generator():
+    gen = card_number_generator(1, 3)
+    assert next(gen) == "0000 0000 0000 0001"
+    assert next(gen) == "0000 0000 0000 0002"
+    assert next(gen) == "0000 0000 0000 0003"
+
+    with pytest.raises(StopIteration):
+        next(gen)
+
+
+def test_one_card_number():
+    gen = card_number_generator(1, 1)
+    assert next(gen) == "0000 0000 0000 0001"
+
+    with pytest.raises(StopIteration):
+        next(gen)
+
+
+def test_invalid_start_stop():
+    gen = card_number_generator(5, 3)
+    assert list(gen) == []
+
+
+def test_large_numbers():
+    gen = card_number_generator(9999999999999998, 9999999999999999)
+    assert next(gen) == "9999 9999 9999 9998"
+    assert next(gen) == "9999 9999 9999 9999"
+
+    with pytest.raises(StopIteration):
+        next(gen)
+
+
+def test_boundary_transition():
+    gen = card_number_generator(9999, 10000)
+    assert next(gen) == "0000 0000 0000 9999"
+    assert next(gen) == "0000 0000 0001 0000"
+
+    with pytest.raises(StopIteration):
+        next(gen)
+
+
+def test_format():
+    gen = card_number_generator(9999, 9999)
+    card_number = next(gen)
+    assert len(card_number) == 19
+    assert card_number.count(" ") == 3
+    assert card_number.replace(" ", "").isdigit()
+
+
+def test_negative_numbers():
+    gen = card_number_generator(-1, 5)
+    with pytest.raises(ValueError) as e:
+        next(gen)
+    assert str(e.value) == "Числа должны быть в диапазоне от 0 до 9999999999999999"
+
+
+def test_too_large_numbers():
+    gen = card_number_generator(1, 10000000000000000)
+    with pytest.raises(ValueError) as e:
+        next(gen)
+    assert str(e.value) == "Числа должны быть в диапазоне от 0 до 9999999999999999"
