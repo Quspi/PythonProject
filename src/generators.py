cc7823769624b5d63e9cf1548ee9f379be01f56d
Oleg Tamanov <olegtamanov@gmail.com>
2025-11-24 19:25:27 +0300

    add input validation to card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -15,7 +15,10 @@ def transaction_descriptions(transactions: list[dict]) -> Iterator[Optional[str]
 
 def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
     """Генерирует номера карт в формате "XXXX XXXX XXXX XXXX" в диапазоне от start до stop."""
-    for number in range(start, stop + 1):
-        card_number_str = str(number).zfill(16)
-        formated_card_number = " ".join([card_number_str[i : i + 4] for i in range(0, 16, 4)])
-        yield formated_card_number
+    if start < 0 or stop < 0 or len(str(start)) > 16 or len(str(stop)) > 16:
+        raise ValueError("Числа должны быть в диапазоне от 0 до 9999999999999999")
+    else:
+        for number in range(start, stop + 1):
+            card_number_str = str(number).zfill(16)
+            formated_card_number = " ".join([card_number_str[i : i + 4] for i in range(0, 16, 4)])
+            yield formated_card_number
