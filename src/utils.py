9e3e338a063c2241242879d732b20b242b91fb0e
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-08 15:25:36 +0300

    add load_transactions function

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def load_transactions(file_path: str) -> list[dict]:
+    if not os.path.exists(file_path):
+        return []
+
+    with open(file_path, "r", encoding="UTF-8") as file:
+        try:
+            transactions = json.load(file)
+
+        except json.decoder.JSONDecodeError:
+            return []
+
+    if not isinstance(transactions, list):
+        return []
+
+    return transactions
