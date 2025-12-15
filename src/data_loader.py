f1623ea29096f679a2f313ae6df8dc4f8009d79d
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-15 21:26:54 +0300

    add read_excel_transactions function

--- src/data_loader.py
+++ src/data_loader.py
@@ -25,3 +25,18 @@ def read_csv_transactions(filepath: str, delimiter: str = ",") -> list[dict]:
     except FileNotFoundError as error:
         logger.error(f"Ошибка: {error}: Файл не существует или удален.")
         raise ValueError(f"Файл не найден {filepath}")
+
+
+def read_excel_transactions(filepath: str) -> list[dict]:
+    """Загружает транзакции из Excel файла и возвращает их в формате list[dict]"""
+    try:
+        transactions = pd.read_excel(filepath)
+
+        result = transactions.to_dict(orient="records")
+        logger.info(f"{len(result)} транзакций успешно загружено из файла: {filepath}")
+
+        return result
+
+    except FileNotFoundError as error:
+        logger.error(f"Ошибка: {error}: Файл не существует или удален.")
+        raise ValueError(f"Файл не найден {filepath}")
