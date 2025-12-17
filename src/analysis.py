c470f4f7fbfff660cb7e739422a81dc1eff40941
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-18 01:56:01 +0300

    add filter_by_description function to analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 logger = logging.getLogger("analysis")
 logger.setLevel(logging.DEBUG)
@@ -10,3 +11,21 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
+def filter_by_description(operations: list[dict], search_string: str) -> list[dict]:
+    if not operations:
+        logger.warning(f"Получен пустой список операций {operations}")
+        return []
+
+    logger.info(f"Поиск операций по {search_string} в описании.")
+    pattern = re.escape(search_string)
+
+    result = []
+
+    for operation in operations:
+        description = operation.get("description")
+        if isinstance(description, str) and re.search(pattern, description, flags=re.I):
+            result.append(operation)
+
+    logger.info(f"Найдено {len(result)} совпадений")
+
+    return result
