7df88320dad5e40298c4edf3ddd6acbd18e2a247
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-20 12:12:34 +0300

    add configuration file config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+JSON_PATH = os.path.join(BASE_DIR, "data", "operations.json")
+CSV_PATH = os.path.join(BASE_DIR, "data", "transactions.csv")
+EXCEL_PATH = os.path.join(BASE_DIR, "data", "transactions_excel.xlsx")
